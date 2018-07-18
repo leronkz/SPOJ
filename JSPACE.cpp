c1372cd43efcd9ef9ba671503593460aeb047e8b
leronkz <41147218+leronkz@users.noreply.github.com>
2018-07-18 11:50:26 +0200

    Spacje

---
+++ JSPACE.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+
+	string napis;
+	while(getline(cin,napis))
+	{
+		for(unsigned int i=0; i<napis.size(); i++)
+		{
+			if(isspace(napis[i]))
+			{
+				napis[i]=0;
+				napis[i+1]=toupper(napis[i+1]);
+			}
+		}
+		for(unsigned int i=0; i<napis.size(); i++)
+		{
+			if(napis[i]!=0)
+			{
+				cout<<napis[i];
+			}
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
